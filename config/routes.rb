8af11036d8f00a91a0b21457c6ced5fd7a0af221
Paul Blackmore <paulmichaelblackmore@gmail.com>
2018-10-29 11:23:16 +0100

    setup user pages and devise forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'simple_pages/index'
   resources :messages
   resources :rooms
@@ -7,4 +8,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'simple_pages#index'
+  mount ActionCable.server => '/cable'
 end

cd798e343a8a7a9259af954d0894138350c674aa
Paul Blackmore <paulmichaelblackmore@gmail.com>
2018-10-30 17:10:27 +0100

    fully respsonsive

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,4 @@
 $(document).on('turbolinks:load', function(){
 	$('.notice').slideDown('fast').show().delay(3000).slideUp('fast');
-	$('.alert').SlideDown('fast').show().delay(3000).slideUp('fast');
+	$('.alert').slideDown('fast').show().delay(3000).slideUp('fast');
 });

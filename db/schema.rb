b5148987708bb14fb813771f1645dfffb9c69adc
Paul Blackmore <paulmichaelblackmore@gmail.com>
2018-10-30 14:36:33 +0100

    appearance test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_29_131651) do
+ActiveRecord::Schema.define(version: 2018_10_30_132230) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2018_10_29_131651) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.boolean "admin", default: false, null: false
+    t.boolean "online", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

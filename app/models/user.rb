4bae2bcf9cdc1f85344497f5d83ca822f5acb957
Paul Blackmore <paulmichaelblackmore@gmail.com>
2018-10-31 17:23:16 +0100

    default user image added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
 	has_one_attached :profile_pic
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable

9e1357360e28d2929aac2f398ebc09018dfeb190
Paul Blackmore <paulmichaelblackmore@gmail.com>
2018-11-04 15:53:22 +0100

    users mage showing when comment posted

--- db/schema.rb
+++ db/schema.rb
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(version: 2018_10_30_165731) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.boolean "admin", default: false, null: false
-    t.boolean "online", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

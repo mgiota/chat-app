566973155115949d2900ba3e4c847c44851782a6
giota-cliqz <panagiota@cliqz.com>
2018-11-07 10:42:23 +0100

    complete membership

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -11,22 +11,25 @@ class MembershipsController < ApplicationController
     @room = Room.find(params[:room_id])
     redirect_with_flash unless member_of_group
 
-    user = User.find(params[:user_id])
-    puts user
-    membership = Membership.new(user_id: user.id,
-                                room_id: @room.id)
+    @user = User.find(params[:membership][:user_id])
+    @membership = Membership.new(user_id: @user.id,
+                            room_id: @room.id)
 
-    # respond_to do |format|
-      if membership.save
-        json_hash = {email: user.email, id: user.id}
+
+    respond_to do |format|
+      if @membership.save
+        #json_hash = {email: user.email, id: user.id}
         #render text: JSON.generate(json_hash)
-        render json: json_hash
-        # format.html { redirect_to @room, notice: 'Review was created successfully.' }
-        # format.json { render :show, status: :created, location: @room }
-        # format.js
+        #render json: json_hash
+        format.html { redirect_to @room, notice: 'Review was created successfully.' }
+        format.json { render :show, status: :created, location: @room }
+        format.js
       else
         #error handling
+        format.html { redirect_to @room, alert: 'User was not added to the room.' }
+        format.json { render json: @membership.errors, status: :unprocessable_entity }
+
       end
-    # end
+    end
   end
 end
